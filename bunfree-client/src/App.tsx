4104d0e00e3d57e6bf1ace8f7d53f8c6bc734d54
Yosei Ushida <ushida.yosei@gmail.com>
2025-04-25 03:05:18 +0900

    PWAプロンプトを非表示

--- bunfree-client/src/App.tsx
+++ bunfree-client/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ChatApp from './components/ChatApp';
 import FavoritePage from './components/FavoritePage';
-import PWAInstallPrompt from './components/PWAInstallPrompt';
+// import PWAInstallPrompt from './components/PWAInstallPrompt';
 import './App.css';
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
           <Route path="/" element={<ChatApp />} />
           <Route path="/favorites" element={<FavoritePage />} />
         </Routes>
-        <PWAInstallPrompt />
+        {/* <PWAInstallPrompt /> */}
       </div>
     </Router>
   );
